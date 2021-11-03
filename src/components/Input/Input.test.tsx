6cf3815657d0a0d3c665dbc54ce5285ff781bc66
stoyan.kalinov <stoyan.kalinov@mobilewaves.com>
2021-11-03 09:55:01 +0200

    add additional test for the radio input

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -32,18 +32,37 @@ describe("Input component", () => {
         [InputType.password, "password label", ""],
         [InputType.radio, "radio label", "checked value"],
         [InputType.checkbox, "checkbox label", "checked value"],
-    ])("should update input value OR checked property correctly after user interaction", (inputType, labelText, value) => {
+    ])("should update input value AND/OR checked property correctly after user interaction", (inputType, labelText, value) => {
         render(<Input type={inputType} label={labelText} id="test id" name="test name" value={value} />);
 
         const inputComponent: HTMLInputElement = screen.getByLabelText(labelText);
 
         if (inputType === InputType.radio || inputType === InputType.checkbox) {
             userEvent.click(inputComponent);
-            expect(inputComponent.checked).toBe(true);
+            expect(inputComponent).toBeChecked();
             expect(inputComponent.value).toEqual(value);
         } else {
             userEvent.type(inputComponent, "text");
-            expect(inputComponent.value).toEqual("text");
+            expect(inputComponent).toHaveValue("text");
+
+            userEvent.clear(inputComponent);
+            expect(inputComponent).toHaveValue("");
         }
+    });
+
+    it("only one radio button from a group should be checked at a time", () => {
+        render(<Input type={InputType.radio} label="radio1" id="radio1id" name="radioGroup" value="radio1Value" />);
+        render(<Input type={InputType.radio} label="radio2" id="radio2id" name="radioGroup" value="radio2Value" />);
+
+        const radioOneElement: HTMLInputElement = screen.getByLabelText("radio1");
+        const radioTwoElement: HTMLInputElement = screen.getByLabelText("radio2");
+
+        userEvent.click(radioOneElement);
+        expect(radioOneElement).toBeChecked();
+        expect(radioTwoElement).not.toBeChecked();
+
+        userEvent.click(radioTwoElement);
+        expect(radioOneElement).not.toBeChecked();
+        expect(radioTwoElement).toBeChecked();
     });
 });
